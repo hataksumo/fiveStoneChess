dfed8cfc32c222f478e00ec96a8a477ad3ba73d9
chuzhiyong <hataksumo@163.com>
2017-06-21 21:03:16 +0800

    加点代码

--- UnityClient/Assets/Script/Action/Action.cs
+++ UnityClient/Assets/Script/Action/Action.cs
@@ -29,6 +29,40 @@ namespace CCAction
         {
             return "Action";
         }
+    }
 
+    abstract class ActionInstence : Action
+    {
+        public override void step(object v_target, float v_dt)
+        {
+            
+        }
+
+        public abstract void Do();
+
+        public override bool isDone()
+        {
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return "ActionInstence";
+        }
+    }
+
+    abstract class ActionInteval : Action
+    {
+        protected float m_time;
+        public ActionInteval()
+        {
+            m_time = 0;
+        }
+        public virtual void start(Object v_target)
+        {
+            m_time = 0;
+            onStart(v_target);
+        }
+        protected abstract void onStart(Object v_target);
     }
 }
