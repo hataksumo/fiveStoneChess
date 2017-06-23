d22ba62fe73bb39f4e574515ff3249c2b804d9bb
织法酱 <hataksumo@163.com>
2017-06-23 09:24:30 +0800

    action

--- UnityClient/Assets/Script/Action/Action.cs
+++ UnityClient/Assets/Script/Action/Action.cs
@@ -17,7 +17,7 @@ namespace CCAction
 
         }
 
-        public void initWithTarget(Object v_target)
+        public virtual void initWithTarget(Object v_target)
         {
 
         }
@@ -63,6 +63,61 @@ namespace CCAction
             m_time = 0;
             onStart(v_target);
         }
-        protected abstract void onStart(Object v_target);
+        public override void step(Object v_target, float v_dt)
+        {
+            m_time += v_dt;
+            onStep(v_target, v_dt);
+        }
+        protected abstract void onStep(Object v_target,float v_dt);
+    }
+
+    abstract class ActionLerp : ActionInteval
+    {
+        protected float m_duration;
+        protected ILerpStrategyBase m_lerpStrategy;
+        public ActionLerp()
+        {
+            m_duration = 0;
+            m_lerpStrategy = null;
+        }
+        public override void initWithTarget(Object v_target)
+        {
+            if (m_lerpStrategy == null)
+                m_lerpStrategy = new LinearLerp();
+            onInitWithTarget(v_target);
+        }
+        protected abstract void onInitWithTarget(Object v_target);
+        protected override void onStep(object v_target,float v_dt)
+        {
+            float t = m_time / m_duration;
+            update(v_target, t);
+        }
+        public void update(object v_target,float v_t)
+        {
+            float tt = m_lerpStrategy.getLerpVar(v_t);
+            onUpdate(v_target, v_t);
+        }
+        protected abstract void onUpdate(object v_target, float v_t);
+        public override bool isDone()
+        {
+            return m_time >= m_duration;
+        }
+
+        public float Duration
+        {
+            get { return m_duration; }
+        }
+
+        public ILerpStrategyBase LerpStrategy
+        {
+            get { return m_lerpStrategy; }
+            set { m_lerpStrategy = value; }
+        }
+
+        public override string ToString()
+        {
+            return "ActionLerp";
+        }
+
     }
 }
